360e264afd41888d1473218aad1e8d5196bcc000
dovricarbhoy@gmail.com <dovricarbhoy@gmail.com>
2025-05-05 20:00:22 -0500

    Refactor violin plot to include median display and adjust styling

--- mtpl_3.py
+++ mtpl_3.py
@@ -7,13 +7,11 @@ df_msc = df.loc[df['Sexo'] == 'Masculino']
 df_fem = df.loc[df['Sexo'] == 'Femenino']
 # print(df_fem)
 
-fig, ax = plt.subplots(figsize=(5,4))
-violin_parts = ax.violinplot(df_fem['Altura (cm)'])
+plt.style.use('seaborn-v0_8-white')
 
-for pc in violin_parts['bodies']:
-    pc.set_facecolor('orchid')  # Cambia 'lightblue' por el color deseado
-    pc.set_edgecolor('blue')      # Cambia 'blue' por el color del borde
-    pc.set_alpha(0.5)    
+fig, ax = plt.subplots(figsize=(5,4))
+violin_parts = ax.violinplot(df_fem['Altura (cm)'], 
+                              showmedians=True)   
 
 ax.set_title('Distribucion de altura de mujeres')
 ax.set_xlabel('Sexo')
